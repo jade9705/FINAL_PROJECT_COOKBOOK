cbe77da15fca2d2557526dba2af13990e17a8cab
unknown <wrannjade@gmail.com>
2020-07-16 16:39:40 +0100

    some more styles

--- resources/views/recipe/recipe.blade.php
+++ resources/views/recipe/recipe.blade.php
@@ -32,65 +32,65 @@
   <div class="allComments">
     
     @foreach($recipe->comments as $comment)
-    @if(!auth()->check())
-    <h1 class="hideThis">hi</h1>
-    @else
+      @if(!auth()->check())
+        <h1 class="hideThis">hi</h1>
+      @else
       @if($comment->user_id  === auth()->user()->id)
-      <h2 class="usercomment"> {{ $userComment }} </h2>
+        <h2 class="usercomment"> {{ $userComment }} </h2>
       @endif
       @endif
     @endforeach
-      @if(!auth()->check())
-        <h2 class="usercomment">Please create an account to leave comments</h2>
-        @else
-        @if(auth()->user()->id == $recipe->user_id)
-          <h2 class="usercomment" >you cannot rate your own recipe, sorry!</h2>
-        @else
-        @if(auth()->user()->id !== $recipe->user_id && $userComment !== 'You have already rated this recipe. Thankyou.' )
-         <h2 class="usercomment">What do you think,  {{ auth()->user()->first_name }}?</h2>
-            <form action='/recipe/{recipe_id}/comment' method="post" class="comment-form">
-              @csrf
-              {{-- success message --}}
-              @if (Session::has('success_message'))
-                <div class="alert alert-success">
-                  {{ Session::get('success_message') }}
-                </div>
-              @endif
-              @if (count($errors) > 0)
-                <div class="alert alert-danger">
-                  <ul>
-                    @foreach ($errors->all() as $error)
-                      <li>{{ $error }}</li>
-                    @endforeach
-                  </ul>
-                </div>
-              @endif
-              <input type="hidden" name='recipe_id' value={{$recipe_id}} />
-              <input type="hidden" name='user_id' value={{$user_id}} />
+    @if(!auth()->check())
+      <h2 class="usercomment">Please create an account to leave comments</h2>
+    @else
+    @if(auth()->user()->id == $recipe->user_id)
+      <h2 class="usercomment" >you cannot rate your own recipe, sorry!</h2>
+    @else
+    @if(auth()->user()->id !== $recipe->user_id && $userComment !== 'You have already rated this recipe. Thankyou.' )
+      <h2 class="usercomment">What do you think,  {{ auth()->user()->first_name }}?</h2>
+        <form action='/recipe/{recipe_id}/comment' method="post" class="comment-form">
+          @csrf
+            {{-- success message --}}
+            @if (Session::has('success_message'))
+              <div class="alert alert-success">
+                {{ Session::get('success_message') }}
+              </div>
+            @endif
+            @if (count($errors) > 0)
+              <div class="alert alert-danger">
+                <ul>
+                  @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                  @endforeach
+                </ul>
+              </div>
+            @endif
+            <input type="hidden" name='recipe_id' value={{$recipe_id}} />
+            <input type="hidden" name='user_id' value={{$user_id}} />
 
-              <div class="form-group">
-                <input type="hidden" name="rating" class="ratingNumber" value="" >
-                <div class="rating">
-                  <div class="rating__value">1</div>
-                  <div class="rating__stars">
-                    <div class="rating__star rating__star--on"></div>
-                    <div class="rating__star "></div>
-                    <div class="rating__star "></div>
-                    <div class="rating__star"></div>
-                    <div class="rating__star"></div>
-                  </div>
+            <div class="form-group">
+              <input type="hidden" name="rating" class="ratingNumber" value="" >
+              <div class="rating">
+                <div class="rating__value">1</div>
+                <div class="rating__stars">
+                  <div class="rating__star rating__star--on"></div>
+                  <div class="rating__star "></div>
+                  <div class="rating__star "></div>
+                  <div class="rating__star"></div>
+                  <div class="rating__star"></div>
                 </div>
               </div>
+            </div>
 
-              <div class="form-group" id="commentText">
-                <label for="">
-                  Text:<br>
-                  <textarea name="text" cols="85" rows="5">{{ old('text') }}</textarea>
-                </label>
-                  @if($errors->has('text'))
-                    {{ $errors->first('text')}}
-                  @endif
-              </div>
+            <div class="form-group" id="commentText">
+              <label for="">
+                Text:<br>
+                <textarea name="text" cols="85" rows="5">{{ old('text') }}</textarea>
+              </label>
+                @if($errors->has('text'))
+                  {{ $errors->first('text')}}
+                @endif
+            </div>
    
               {{-- javascript for star rating--}}
               <script>
@@ -121,51 +121,48 @@
               <div class="form-group">
                 <input type="submit" value="submit" name="submit" class="btn btn-success"/>
               </div>
-          </form>
-@endif 
-@endif
+        </form>
+      @endif 
+    @endif
 
-@endif
+    @endif
 
-<div class="review">
+    <div class="review">
 
-  <h2>Comments...</h2>
-  {{ $commentNumber }}
-  <ul> 
-    @foreach ($recipe->comments as $comment)
-      <div class="recipe-container">
-        <div class="hideThis"> {{$number = (int)$comment->rating}}</div>
-        <img src="/images/uploads/user/{{$comment->user->image_url}}" class="userImage"/>
-        <div class="nameAndText">
-          <li class="userName"> <a href="/profile/{{$comment->user_id}}"><strong> {{$comment->user->first_name}} {{$comment->user->surname}}</strong></a> </li>
-          <div class="stars">
-            @for($i = 0; $i < $number; $i ++)
-              <div id="starRender" class="rating__star rating__star--on"></div>
-            @endfor
-            {{-- <div class="ratingRender"> {{ $comment->rating }}/5</div>   --}}
-          </div>
-          <li class="review">
-            {{ $comment->text }}
-            <br>      
-       
-      </div>
-     
-       
-            @if(auth()->check())
-              @if(auth()->user()->id === $comment->user->id)
-                <form action="{{route('comment.delete', [$recipe->id, $comment->id])}}" method="post" id="deleteButton">
-                  @csrf
-                  @method('delete')
-                  <input type="submit" value="delete" class="btn btn-danger">
-                </form>
-              @endif
-            @endif
-        </li>        
+    <h2>Comments...</h2>
+    {{ $commentNumber }}
+    <ul> 
+      @foreach ($recipe->comments as $comment)
+        <div class="recipe-container">
+          <div class="hideThis"> {{$number = (int)$comment->rating}}</div>
+          <img src="/images/uploads/user/{{$comment->user->image_url}}" class="userImage"/>
+          <div class="nameAndText">
+            <li class="userName"> <a href="/profile/{{$comment->user_id}}"><strong> {{$comment->user->first_name}} {{$comment->user->surname}}</strong></a> </li>
+            <div class="stars">
+              @for($i = 0; $i < $number; $i ++)
+                <div id="starRender" class="rating__star rating__star--on"></div>
+              @endfor
+              {{-- <div class="ratingRender"> {{ $comment->rating }}/5</div>   --}}
+            </div>
+            <li class="review">
+              {{ $comment->text }}
+              <br>      
+            </div>
+              @if(auth()->check())
+                @if(auth()->user()->id === $comment->user->id)
+                    <form action="{{route('comment.delete', [$recipe->id, $comment->id])}}" method="post" id="deleteButton">
+                      @csrf
+                      @method('delete')
+                        <input type="submit" value="delete" class="btn btn-danger">
+                    </form>
+                  @endif
+                @endif
+          </li>        
           <p class="commentTime">{{ $comment->created_at }}</P>
-      </div>
-    @endforeach
-  </ul>
-</div> 
+        </div>
+      @endforeach
+    </ul>
+  </div> 
 </div>
 </div>
 </div>
